0abadd89b7cb5152a283f9f283bfa98905c5047c
Franz Knupfer <franzknupfer@hotmail.com>
2016-01-06 11:00:20 -0800

    configure redcarpet markdown options in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,12 @@ module ApplicationHelper
     options = {
       hard_wrap:       true,
       link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
+      space_after_headers: true
     }
 
     extensions = {
       autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
+      superscript:        true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
